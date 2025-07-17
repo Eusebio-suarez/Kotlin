4542b1ac7bdec9d9ce5dd473c3f0974680f8e09c
eusebio suarez <eusebiosuaresmartines@gmail.com>
2025-07-16 23:43:52 -0500

    primera app con 2 viewa que se transfieren una variable como informacion

--- app/src/main/java/com/eusebio/android/firstapp/firstAppActivity.kt
+++ app/src/main/java/com/eusebio/android/firstapp/firstAppActivity.kt
@@ -1,8 +1,12 @@
 package com.eusebio.android.firstapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
+import androidx.appcompat.widget.AppCompatEditText
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.eusebio.android.R
@@ -16,6 +20,27 @@ class firstAppActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        //variable que representa a el boton
+        val btnStart = findViewById<AppCompatButton>(R.id.btnStart)
+
+        //variable que representa ea el campo de texto
+        val text = findViewById<AppCompatEditText>(R.id.eText)
+        //agregar el evento de click a el boton
+        btnStart.setOnClickListener {
+            //convertir el input en texto
+            var editText= text.text.toString()
+
+            //validar que el campo no este vacio
+            if (editText.isNotEmpty()){
+                //permite establecer una navegacion entre paginas
+                var intent = Intent(this, resultActivity::class.java)
+                //enviar informacion estra entre views
+                intent.putExtra("EXTRA_NAME",editText)
+                startActivity(intent)
+            }
+
         }
     }
 }
---
+++ app/src/main/java/com/eusebio/android/firstapp/resultActivity.kt
@@ -0,0 +1,30 @@
+package com.eusebio.android.firstapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.eusebio.android.R
+
+class resultActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_result)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        //elemento testo
+        val textResult = findViewById<TextView>(R.id.textResult)
+
+        //recuperar el valor del nombre
+        val name = intent.extras?.getString("EXTRA_NAME")
+
+        textResult.text = "|Bienvenido $name!"
+    }
+}
